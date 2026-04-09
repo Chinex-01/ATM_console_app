c063a2ed495c2b681c6261a070beffc7edfdcb55
Nonso <onyeokorokennedy@gmail.com>
2026-04-09 13:58:39 +0100

    push

--- ATM console app/Program.cs
+++ ATM console app/Program.cs
@@ -1,176 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using ATM_console_app; 
+
 namespace nonso
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // register
             Console.WriteLine(" Welcome to First bank!! ");
             Console.WriteLine(" Do you have an account with us ");
-            Console.WriteLine(1 + ". yes");
-            Console.WriteLine(2 + ". No");
+            Console.WriteLine("1. Yes");
+            Console.WriteLine("2. No");
+
             int answer = Convert.ToInt32(Console.ReadLine());
-            int yeah = 1;
-            int nah = 2;
-            if (answer == yeah)
-            {
-                Console.Write(" Enter your account number: ");
-                var accountnumber = Console.ReadLine();
-                Console.Write(" Enter your 4 digits pin: ");
-                int password = Convert.ToInt32(Console.ReadLine());
-            }
-            else if (answer == nah)
-            {
-                Console.Write(" Please enter your name: ");
-                string name = Console.ReadLine();
-                Console.WriteLine();
-                Console.Write(" Please enter your email address: ");
-                string email = Console.ReadLine();
-                Console.WriteLine();
-                Console.Write(" Enter your phone number: ");
-                string phonenumber = Console.ReadLine();
-                Console.WriteLine();
-                Console.Write(" Enter your bvn: ");
-                var bvn = Console.ReadLine();
-                Console.WriteLine();
-                Console.Write(" Please create a pin: ");
-                int password = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine(" your datails : " + name + " " + email + ", " + phonenumber + " , " + password + " ," + bvn + " .");
-            }
-            else
-            {
-                Console.Write(" error, pls enter a number from (1 - 3)");
-            }
-           
-            bool yes = true;
-            bool no = false;
-            double balance = 0.00;
 
-
-            while (yes)
+            if (answer == 1)
             {
-                int deposit = 1;
-                int checkbalance = 2;
-                int withdraw = 3;
-                int changepin = 4;
-                int phonenumeber = 5;
-                int choice;
-
-                Console.WriteLine(" Which service do you want ");
-                Console.WriteLine(1 + " Deposit");
-                Console.WriteLine(2 + " check balance");
-                Console.WriteLine(3 + " Withdraw ");
-                Console.WriteLine(4 + " change pin");
-                Console.WriteLine(5 + " update phone number");
-
-                choice = Convert.ToInt32(Console.ReadLine());
-                if (choice == deposit) // Deposit (only accepts 1000 naira notes)
+                List<Customer> customers = new List<Customer>()
+    {
+        new Customer { Name = "John", AccountNumber = "2301110001", Pin = 1234, ID = 1},
+        new Customer { Name = "Mary", AccountNumber = "2301110002", Pin = 2222, ID = 2 },
+        new Customer { Name = "David", AccountNumber = "2301110003", Pin = 3333, ID = 3 },
+        new Customer { Name = "Sarah", AccountNumber = "2301110004", Pin = 4444, ID = 4},
+        new Customer { Name = "James", AccountNumber = "2301110005", Pin = 5555, ID = 5},
+    };
+                Console.Write("Enter your account number: ");
+                string? accountnumber = Console.ReadLine();
+                Customer? Custoomer = null;
+                foreach (var customer in customers)
                 {
-                    do
+                    if (customer.AccountNumber == accountnumber)
                     {
-                        Console.Write(" Enter the amount you want to deposit ?: ");
-                        double amount = Convert.ToDouble(Console.ReadLine());
-                        if (amount > 0 && amount % 1000 == 0)
-                        {
-                            balance += amount;
-                            Console.WriteLine("deposit successfully");
-                            Console.WriteLine("current balance: " + balance);
-                        }
-                        else
-                        {
-                            Console.WriteLine("invalid amount (must be in 1000 notes)");
-                        }
+                        Custoomer = customer;
                         break;
-                    } while (choice == deposit);
-                }
-                else if (choice == checkbalance) // Check balance
-                {
-                    do
-                    {
-                        Console.WriteLine("Balance : " + balance);
-                        break;
-                    } while (choice == checkbalance);
-                }
-                else if (choice == withdraw)  // withdraw (accepts only 100 notes)
-                {
-                    do
-                    {
-                        Console.Write(" Enter the amount you want to withdraw ?: ");
-                        double amount = Convert.ToDouble(Console.ReadLine());
-                        if (amount > 0 && amount % 1000 == 0 && amount <= balance)
-                        {
-                            balance -= amount;
-                            Console.WriteLine("withdrawal successful");
-                            Console.WriteLine("current balance: " + balance);
-                        }
-                        else
-                        {
-                            Console.WriteLine("invalid amount");
-                        }
-                        break;
-                    } while (choice == withdraw);
-                }
-                else if (choice == changepin) // change pin 
-                {
-                    // change pin
-                    string currentpin = "1234";
-                    Console.Write(" Enter your current pin: ");
-                    string currentpininput = Convert.ToString(Console.ReadLine());
-                    if (currentpininput != currentpin) Console.WriteLine(" incorrect pin.");
-                    string newPin;
-                    string newPin2;
-                    do
-                    {
-                        Console.Write("Enter new PIN: ");
-                        newPin = Console.ReadLine();
-                        Console.Write("Retype new PIN: ");
-                        newPin2 = Console.ReadLine();
-                        if (newPin != newPin2) Console.WriteLine(" Don't match, Try again.");
-                    } while (newPin != newPin2);
-                    string currentPin = newPin;
-                    Console.WriteLine(" success, New PIN: " + newPin);
-                }
-                else if (choice == phonenumeber)
-                {
-                    Console.WriteLine("Enter your old number: ");
-                    string oldnum = Console.ReadLine();
-
-                    Console.WriteLine("Enter your new number: ");
-                    string newnum = Console.ReadLine();
-                 
-                    Console.WriteLine("Re-enter your new number: ");
-                    string newnum1 = Console.ReadLine();
-
-                    Console.WriteLine( " ");
-
-                    if (newnum == newnum1)
-                    {
-                        Console.WriteLine(" your change was sucessfully ");
                     }
-                    else
-                    {
-                        Console.WriteLine("Both don't match ");
+                }
+                if (Custoomer != null)
+                {
+                      Console.Write("Enter your 4-digit pin: ");
+                        int password = Convert.ToInt32(Console.ReadLine());
+
+                        if (Custoomer.Pin == password)
+                        {
+                            Console.WriteLine("Login successful");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Incorrect pin, try again");
+                            Console.WriteLine("Restart your app");
+                        return;
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Invalid number, Enter number from (1- 4)");
+                    Console.WriteLine("You don't have an account with us.");
+                    Console.WriteLine("Restart your app");
+                    return;
                 }
-
-                Console.WriteLine(" DO you want to use any of our services (yes/no)");
-                string answerr = Console.ReadLine();
-
-                if (answerr == "no")
-                {
-                    Console.WriteLine("Thank you for banking with us ");
-                    Console.WriteLine("Have a nice day!!");
-                    break; 
-                }
-               
             }
+            else if (answer == 2)
+            {
+                Console.Write("Please enter your name: ");
+                string name = Console.ReadLine();
 
-        }
+                Console.Write("Please enter your email address: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Enter your phone number: ");
+                string phoneNumber = Console.ReadLine();
+
+                Console.Write("Enter your BVN: ");
+                string bvn = Console.ReadLine();
+
+                Console.Write("Please create a pin: ");
+                int password = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"Your details: {name}, {email}, {phoneNumber}, {password}, {bvn}");
+                Console.WriteLine("Registration successful!");
+            }
+            else
+            {
+                Console.WriteLine("you entered a wrong input, restart and enter 1 or 2.");
+                return;
+            }
+                Console.WriteLine();
+                bool yes = true;
+                bool no = false;
+                while (yes)
+                {
+                Services.Service();
+                string answerr = Console.ReadLine();
+                    if (answerr == "no")
+                    {
+                        Console.WriteLine("Thank you for banking with us ");
+                        Console.WriteLine("Have a nice day!!");
+                        break;
+                    }
+                }
+            }
     }
 }
