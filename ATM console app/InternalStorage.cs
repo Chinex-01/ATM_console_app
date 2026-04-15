d7a884271711c9fd2f42a018b834fe6dd61cf653
Chinex-01 <kennedynonso001@gmail.com>
2026-04-15 09:00:09 +0100

    push

---
+++ ATM console app/InternalStorage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM_console_app
+{
+    public class InternalStoragee
+    {
+        public List<Customer> ? Storage;
+    }
+}
--- ATM console app/Program.cs
+++ ATM console app/Program.cs
@@ -1,105 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
-using ATM_console_app; 
+using System.Runtime.CompilerServices;
+using ATM_console_app;
 
-namespace nonso
+
+Console.WriteLine(" Welcome to First bank!! ");
+Console.WriteLine(" Do you have an account with us ");
+Console.WriteLine("1. Yes");
+Console.WriteLine("2. No");
+
+int answer = Convert.ToInt32(Console.ReadLine());
+
+if (answer == 1)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(" Welcome to First bank!! ");
-            Console.WriteLine(" Do you have an account with us ");
-            Console.WriteLine("1. Yes");
-            Console.WriteLine("2. No");
+    List<Customer> customers = new List<Customer>()
 
-            int answer = Convert.ToInt32(Console.ReadLine());
-
-            if (answer == 1)
-            {
-                List<Customer> customers = new List<Customer>()
     {
-        new Customer { Name = "John", AccountNumber = "2301110001", Pin = 1234, ID = 1},
-        new Customer { Name = "Mary", AccountNumber = "2301110002", Pin = 2222, ID = 2 },
-        new Customer { Name = "David", AccountNumber = "2301110003", Pin = 3333, ID = 3 },
-        new Customer { Name = "Sarah", AccountNumber = "2301110004", Pin = 4444, ID = 4},
-        new Customer { Name = "James", AccountNumber = "2301110005", Pin = 5555, ID = 5},
+        new Customer { Name = "John", AccountNumber = "2301110001", Pin = 1234},
+        new Customer { Name = "Mary", AccountNumber = "2301110002", Pin = 2222},
+        new Customer { Name = "David", AccountNumber = "2301110003", Pin = 3333},
+        new Customer { Name = "Sarah", AccountNumber = "2301110004", Pin = 4444},
+        new Customer { Name = "James", AccountNumber = "2301110005", Pin = 5555},
     };
-                Console.Write("Enter your account number: ");
-                string? accountnumber = Console.ReadLine();
-                Customer? Custoomer = null;
-                foreach (var customer in customers)
-                {
-                    if (customer.AccountNumber == accountnumber)
-                    {
-                        Custoomer = customer;
-                        break;
-                    }
-                }
-                if (Custoomer != null)
-                {
-                      Console.Write("Enter your 4-digit pin: ");
-                        int password = Convert.ToInt32(Console.ReadLine());
 
-                        if (Custoomer.Pin == password)
-                        {
-                            Console.WriteLine("Login successful");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Incorrect pin, try again");
-                            Console.WriteLine("Restart your app");
-                        return;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("You don't have an account with us.");
-                    Console.WriteLine("Restart your app");
-                    return;
-                }
-            }
-            else if (answer == 2)
-            {
-                Console.Write("Please enter your name: ");
-                string name = Console.ReadLine();
+    Console.Write("Enter your account number: ");
+    string? accountnumber = Console.ReadLine();
+    Customer? Custoomer = null;
+    foreach (var customer in customers)
+    {
+        if (customer.AccountNumber == accountnumber)
+        {
+            Custoomer = customer;
+            break;
+        }
+    }
+    if (Custoomer != null)
+    {
+        Console.Write("Enter your 4-digit pin: ");
+        int password = Convert.ToInt32(Console.ReadLine());
 
-                Console.Write("Please enter your email address: ");
-                string email = Console.ReadLine();
-
-                Console.Write("Enter your phone number: ");
-                string phoneNumber = Console.ReadLine();
-
-                Console.Write("Enter your BVN: ");
-                string bvn = Console.ReadLine();
-
-                Console.Write("Please create a pin: ");
-                int password = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine($"Your details: {name}, {email}, {phoneNumber}, {password}, {bvn}");
-                Console.WriteLine("Registration successful!");
-            }
-            else
-            {
-                Console.WriteLine("you entered a wrong input, restart and enter 1 or 2.");
-                return;
-            }
-                Console.WriteLine();
-                bool yes = true;
-                bool no = false;
-                while (yes)
-                {
-                Services.Service();
-                string answerr = Console.ReadLine();
-                    if (answerr == "no")
-                    {
-                        Console.WriteLine("Thank you for banking with us ");
-                        Console.WriteLine("Have a nice day!!");
-                    Console.WriteLine("hfggu");
-                        break;
-                    }
-                }
-            }
+        if (Custoomer.Pin == password)
+        {
+            Console.WriteLine("Login successful");
+        }
+        else
+        {
+            Console.WriteLine("Incorrect pin, try again");
+            Console.WriteLine("Restart your app");
+            return;
+        }
+    }
+    else
+    {
+        Console.WriteLine("You don't have an account with us.");
+        Console.WriteLine("Restart your app");
+        return;
     }
 }
+
+else if (answer == 2)
+
+{
+    Console.Write("Please enter your name: ");
+    string Name = Console.ReadLine();
+
+    Console.Write("Please enter your email address: ");
+    string Email = Console.ReadLine();
+
+    Console.Write("Enter your phone number: ");
+    string phoneNumber = Console.ReadLine();
+
+    Console.Write("Enter your BVN: ");
+    string bvn = Console.ReadLine();
+
+    Console.Write("Please create a pin: ");
+    int password = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"Your details: {Name}, {Email}, {phoneNumber}, {password}, {bvn}");
+
+    string AccountNumber = GenerateAccountNumber.NewAccountNumber();
+    Console.WriteLine();
+    Console.WriteLine(AccountNumber);
+    Console.WriteLine( );
+    Console.Write("your account number is: " + AccountNumber);
+    Console.WriteLine();
+    Console.WriteLine("Registration successful!");
+    Console.WriteLine( );
+ 
+}
+else
+{
+    Console.WriteLine("you entered a wrong input, restart and enter 1 or 2.");
+    return;
+
+}
+Console.WriteLine();
+Services.Service();
