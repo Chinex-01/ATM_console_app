a9ec82e827a31b5137a68059e79a34c5bd52e678
Nonso <onyeokorokennedy@gmail.com>
2026-04-29 14:54:56 +0100

    push

---
+++ ATM_Console_App/InternalStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace ATM_console_app
+{
+    public class InternalStoragee
+    {
+        private static List<Customer> Storage { get; set; } = new List<Customer>();
+
+        public static void Add(Customer customer)
+        {
+            Storage.Add(customer);
+        }
+    }
+}
