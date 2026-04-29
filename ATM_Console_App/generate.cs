0f611cc20faec7e72a85b47c81133d2d92d220b2
Nonso <onyeokorokennedy@gmail.com>
2026-04-29 14:53:24 +0100

    push

---
+++ ATM_Console_App/generate.cs
@@ -0,0 +1,25 @@
+﻿using ATM_console_app;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace ATM_console_app
+{
+    public static class GenerateAccountNumber
+    {
+        public static string NewAccountNumber()
+        {
+            const string numberStatic = "230111";
+            Random rand = new Random();
+            string accId = "";
+            for (int i = 0; i < 4; i++)
+            {
+                int randNumber = rand.Next(0, 9);
+                accId += randNumber.ToString();
+            }
+            string UserAccountNumber = numberStatic + accId;
+            return $"{UserAccountNumber}";
+        }
+
+
+    }
+}
